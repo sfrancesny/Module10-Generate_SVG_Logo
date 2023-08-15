24e1c3a1f2b21fe49e2baa802b7f01a57f09a3f1
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-15 13:33:27 -0400

    created index.js file

---
+++ index.js
@@ -0,0 +1,53 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateCircle from './lib/circle';
+import generateTriangle from './lib/triangle';
+import generateSquare from './lib/square';
+import generateLogo from './lib/shapes';
+
+const promptUser = async () => {
+  const userInput = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters:',
+      validate: input => input.length <= 3 || 'Text must be up to three characters',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color (color keyword or hexadecimal number):',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter shape color (color keyword or hexadecimal number):',
+    },
+  ]);
+
+  let shapeSVG = '';
+  if (userInput.shape === 'circle') {
+    shapeSVG = generateCircle(userInput.shapeColor);
+  } else if (userInput.shape === 'triangle') {
+    shapeSVG = generateTriangle(userInput.shapeColor);
+  } else if (userInput.shape === 'square') {
+    shapeSVG = generateSquare(userInput.shapeColor);
+  }
+
+  const svgContent = generateLogo(
+    userInput.text,
+    userInput.textColor,
+    shapeSVG
+  );
+
+  fs.writeFileSync('logo.svg', svgContent);
+  console.log('Generated logo.svg');
+};
+
+promptUser();
