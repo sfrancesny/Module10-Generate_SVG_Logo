a4c778c508309c87b770e7e1830ba6032b3b7c38
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-18 01:07:04 -0400

    add file for logoGenerator / updated function

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
+// index.js
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { createLogoSVG } = require('./logoGenerator'); // You need to implement the logo generation function
+const { createLogoSVG } = require('./lib/logoGenerator'); //
+const { Triangle, Circle, Square } = require('./lib/shapes');
 
 async function generateLogo() {
   const userInput = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter up to three characters:',
-      validate: input => input.length <= 3 ? true : 'Please enter up to three characters.'
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter text color (color keyword or hexadecimal number):'
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Choose a shape:',
-      choices: ['circle', 'triangle', 'square']
-    },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter shape color (color keyword or hexadecimal number):'
-    }
-  ]);
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters:',
+        validate: input => input.length <= 3 ? true : 'Please enter up to three characters.'
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter text color (color keyword or hexadecimal number):'
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square']
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (color keyword or hexadecimal number):'
+      }
+ ]);
 
-  // Generate SVG based on user input
   const svgContent = createLogoSVG(userInput);
 
-  // Write SVG content to a file
   fs.writeFileSync('logo.svg', svgContent);
 
   console.log('Generated logo.svg');
