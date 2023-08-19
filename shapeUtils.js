300af7141af31bce7d72eb95c2bb146ed3bf0868
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-19 00:12:13 -0400

    added logic

--- shapeUtils.js
+++ shapeUtils.js
@@ -1,9 +1,20 @@
 // shapeUtils.js
 
 export function createShapeInstance(shape, color) {
-  // Your shape creation logic here
+  // Replace this with your actual shape creation logic
+  return {
+    type: shape,
+    color: color,
+    // ... additional shape properties
+  };
 }
 
 export function generateSVG(text, textColor, shapeInstance) {
-  // Your SVG generation logic here
+  // Replace this with your actual SVG generation logic
+  const svgContent = `<svg width="300" height="200">
+    <text x="50" y="100" fill="${textColor}">${text}</text>
+    <!-- Shape drawing logic based on shapeInstance -->
+  </svg>`;
+  
+  return svgContent;
 }
