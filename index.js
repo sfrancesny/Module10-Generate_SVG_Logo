1adfd642e317e4a176c78b55699595dccc765cc0
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-18 23:53:01 -0400

    alter

--- index.js
+++ index.js
@@ -2,35 +2,31 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Triangle, Square, Circle } = require("./lib/shapes");
-import("inquirer")
-  .then((inquirer) => {
-    const fs = require("fs");
-    const { Triangle, Square, Circle } = require("./lib/shapes");
 
-    inquirer
-      .prompt([
-        {
-          name: "text",
-          message: "Enter up to three characters:",
-          validate: (input) => input.length <= 3 || "Enter up to three characters",
-        },
-        {
-          name: "textColor",
-          message: "Enter text color (color keyword or hexadecimal number):",
-        },
-        {
-          type: "list",
-          name: "shape",
-          message: "Choose a shape:",
-          choices: ["circle", "triangle", "square"],
-        },
-        {
-          name: "shapeColor",
-          message: "Enter shape color (color keyword or hexadecimal number):",
-        },
-      ])
-      .then((answers) => {
-        const { text, textColor, shape, shapeColor } = answers;
+inquirer
+  .prompt([
+    {
+      name: "text",
+      message: "Enter up to three characters:",
+      validate: (input) => input.length <= 3 || "Enter up to three characters",
+    },
+    {
+      name: "textColor",
+      message: "Enter text color (color keyword or hexadecimal number):",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape:",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      name: "shapeColor",
+      message: "Enter shape color (color keyword or hexadecimal number):",
+    },
+  ])
+  .then((answers) => {
+    const { text, textColor, shape, shapeColor } = answers;
         const shapeInstance = createShapeInstance(shape, shapeColor);
     
         const svgContent = generateSVG(text, textColor, shapeInstance);
@@ -45,7 +41,6 @@ import("inquirer")
       })
       .catch((error) => {
         console.error("Error:", error);
-      });
   })
   .catch((error) => {
     console.error("Error:", error);
