72538bf8de06c4e28a96eaa8f8ddba9524ffcae8
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-18 23:29:43 -0400

    altered to run application & import/export modules

--- index.js
+++ index.js
@@ -1,55 +1,68 @@
-import fs from 'fs';
-import inquirer from 'inquirer';
-import { createLogoSVG } from './lib/logoGenerator.js';
-import { Triangle, Circle, Square } from './lib/shapes.js';
+// index.js
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
-class LogoGenerator {
-  constructor() {
-    this.inquirer = inquirer;
-    this.fs = fs;
-    this.shapes = {
-      triangle: Triangle,
-      circle: Circle,
-      square: Square
-    };
-  }
+inquirer
+  .prompt([
+    {
+      name: "text",
+      message: "Enter up to three characters:",
+      validate: (input) => input.length <= 3 || "Enter up to three characters",
+    },
+    {
+      name: "textColor",
+      message: "Enter text color (color keyword or hexadecimal number):",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape:",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+      name: "shapeColor",
+      message: "Enter shape color (color keyword or hexadecimal number):",
+    },
+  ])
+  .then((answers) => {
+    const { text, textColor, shape, shapeColor } = answers;
+    const shapeInstance = createShapeInstance(shape, shapeColor);
 
-  async generateLogo() {
-    const userInput = await this.inquirer.prompt([
-      {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to three characters:',
-        validate: input => input.length <= 3 ? true : 'Please enter up to three characters.'
-      },
-      {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter text color (color keyword or hexadecimal number):'
-      },
-      {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape:',
-        choices: Object.keys(this.shapes)
-      },
-      {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter shape color (color keyword or hexadecimal number):'
+    const svgContent = generateSVG(text, textColor, shapeInstance);
+
+    fs.writeFile("logo.svg", svgContent, (err) => {
+      if (err) {
+        console.error("Error creating logo.svg:", err);
+      } else {
+        console.log("Generated logo.svg");
       }
-    ]);
+    });
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
 
-    const ShapeClass = this.shapes[userInput.shape];
-    const shapeInstance = new ShapeClass();
-
-    const svgContent = createLogoSVG(userInput, shapeInstance);
-
-    this.fs.writeFileSync('logo.svg', svgContent);
-
-    console.log('Generated logo.svg');
+function createShapeInstance(shape, color) {
+  switch (shape) {
+    case "triangle":
+      return new Triangle(color);
+    case "square":
+      return new Square(color);
+    case "circle":
+      return new Circle(color);
+    default:
+      throw new Error("Invalid shape");
   }
 }
 
-const logoGenerator = new LogoGenerator();
-logoGenerator.generateLogo();
+function generateSVG(text, textColor, shape) {
+  const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <rect width="100%" height="100%" fill="${shape.color}" />
+      <text x="50%" y="50%" dy="0.35em" text-anchor="middle" fill="${textColor}">${text}</text>
+    </svg>
+  `;
+
+  return svgContent;
+}
