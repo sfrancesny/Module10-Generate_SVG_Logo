c2ab040798dc8bdbad895f483268d552c6e18e96
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-19 00:10:40 -0400

    created file to help with SVG logic

---
+++ shapeUtils.js
@@ -0,0 +1,9 @@
+// shapeUtils.js
+
+export function createShapeInstance(shape, color) {
+  // Your shape creation logic here
+}
+
+export function generateSVG(text, textColor, shapeInstance) {
+  // Your SVG generation logic here
+}
