2e560dd287161edf43eb615cd52d0fa23a881c9e
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-08-19 00:00:20 -0400

    use async function

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
-// index.js
-const inquirer = require("inquirer");
-const fs = require("fs");
-const { Triangle, Square, Circle } = require("./lib/shapes");
+(async () => {
+  try {
+    const inquirer = await import("inquirer");
+    const fs = require("fs");
+    const { Triangle, Square, Circle } = await import("./lib/shapes");
 
-inquirer
-  .prompt([
-    {
-      name: "text",
-      message: "Enter up to three characters:",
-      validate: (input) => input.length <= 3 || "Enter up to three characters",
-    },
-    {
-      name: "textColor",
-      message: "Enter text color (color keyword or hexadecimal number):",
-    },
-    {
-      type: "list",
-      name: "shape",
-      message: "Choose a shape:",
-      choices: ["circle", "triangle", "square"],
-    },
-    {
-      name: "shapeColor",
-      message: "Enter shape color (color keyword or hexadecimal number):",
-    },
-  ])
-  .then((answers) => {
+    const answers = await inquirer.prompt([
+      {
+        name: "text",
+        message: "Enter up to three characters:",
+        validate: (input) => input.length <= 3 || "Enter up to three characters",
+      },
+      {
+        name: "textColor",
+        message: "Enter text color (color keyword or hexadecimal number):",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "Choose a shape:",
+        choices: ["circle", "triangle", "square"],
+      },
+      {
+        name: "shapeColor",
+        message: "Enter shape color (color keyword or hexadecimal number):",
+      },
+    ]);
+
     const { text, textColor, shape, shapeColor } = answers;
-        const shapeInstance = createShapeInstance(shape, shapeColor);
-    
-        const svgContent = generateSVG(text, textColor, shapeInstance);
-    
-        fs.writeFile("logo.svg", svgContent, (err) => {
-          if (err) {
-            console.error("Error creating logo.svg:", err);
-          } else {
-            console.log("Generated logo.svg");
-          }
-        });
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-  })
-  .catch((error) => {
+    const shapeInstance = createShapeInstance(shape, shapeColor);
+
+    const svgContent = generateSVG(text, textColor, shapeInstance);
+
+    fs.writeFile("logo.svg", svgContent, (err) => {
+      if (err) {
+        console.error("Error creating logo.svg:", err);
+      } else {
+        console.log("Generated logo.svg");
+      }
+    });
+  } catch (error) {
     console.error("Error:", error);
-  });
+  }
+})();
